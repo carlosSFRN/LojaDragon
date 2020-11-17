0277963be2f32aa21834c8744be84e5b6da66a3d
Carlos Eduardo <carlos505monks@hotmail.com>
2020-11-16 23:17:01 -0300

    scaffolding crud quadrinho

---
+++ WebApplication1/Controllers/QuadrinhosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class QuadrinhosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuadrinhosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Quadrinhos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Quadrinho.ToListAsync());
+        }
+
+        // GET: Quadrinhos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quadrinho = await _context.Quadrinho
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (quadrinho == null)
+            {
+                return NotFound();
+            }
+
+            return View(quadrinho);
+        }
+
+        // GET: Quadrinhos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Quadrinhos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Titulo,Descricao,Preco,CaminhoCapa")] Quadrinho quadrinho)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(quadrinho);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quadrinho);
+        }
+
+        // GET: Quadrinhos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quadrinho = await _context.Quadrinho.FindAsync(id);
+            if (quadrinho == null)
+            {
+                return NotFound();
+            }
+            return View(quadrinho);
+        }
+
+        // POST: Quadrinhos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titulo,Descricao,Preco,CaminhoCapa")] Quadrinho quadrinho)
+        {
+            if (id != quadrinho.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(quadrinho);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuadrinhoExists(quadrinho.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quadrinho);
+        }
+
+        // GET: Quadrinhos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quadrinho = await _context.Quadrinho
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (quadrinho == null)
+            {
+                return NotFound();
+            }
+
+            return View(quadrinho);
+        }
+
+        // POST: Quadrinhos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var quadrinho = await _context.Quadrinho.FindAsync(id);
+            _context.Quadrinho.Remove(quadrinho);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuadrinhoExists(int id)
+        {
+            return _context.Quadrinho.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApplication1/Models/Quadrinho.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Quadrinho
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string CaminhoCapa { get; set; }
+    }
+}
